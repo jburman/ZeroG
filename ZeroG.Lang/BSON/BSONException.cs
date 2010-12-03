963b29a257b9356a22768ce79ca13977b2b892dc
Jeremy Burman <jburman@gmail.com>
2010-12-02 22:56:30 -0500

    Initial commit on BSONTokenizer

---
+++ ZeroG.Lang/BSON/BSONException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZeroG.Lang.BSON
+{
+    public class BSONException : Exception
+    {
+        public BSONException(string message)
+            : base(message)
+        {
+        }
+    }
+
+    public class BSONValidationException : BSONException
+    {
+        public BSONValidationException(string message)
+            : base(message)
+        {
+        }
+    }
+}
