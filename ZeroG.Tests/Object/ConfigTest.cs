552ed606cd5d0307a36173a022e49d0395831f4f
Jeremy Burman <jburman@gmail.com>
2012-12-31 23:44:41 -0500

    Added new test. Updated reference library.

---
+++ ZeroG.Tests/Object/ConfigTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZeroG.Data.Object;
+
+namespace ZeroG.Tests.Object
+{
+    [TestClass]
+    public class ConfigTest
+    {
+        [TestMethod]
+        public void DefaultConfigTest()
+        {
+            var config = Config.Default;
+
+            Assert.AreEqual(ConfigurationManager.AppSettings["ObjectServiceDataDir"], config.BaseDataPath);
+        }
+
+        [TestMethod]
+        public void BasePathConfigTest()
+        {
+            var customBasePath = "C:\\Test\\BasePathConfigTest";
+
+            var config = new Config(customBasePath);
+
+            Assert.AreEqual(customBasePath, config.BaseDataPath);
+
+        }
+    }
+}
